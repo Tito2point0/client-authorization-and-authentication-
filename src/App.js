c665a9830a2b6b37f37ff4f8f020c49a416cc8d9
sanjiro <defconx00@gmail.com>
2023-10-04 10:33:34 -0400

    would not work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const Login = () => {
+  return <h2>Login</h2>;
+};
+
+const FriendsList = () => {
+  return <h2>FriendsList</h2>;
+};
+
+const AddFriend = () => {
+  return <h2>AddFriend</h2>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <Route  path="/">
+          <Login />
+        </Route>
+        <Route path="/Login">
+          <Login />
+        </Route>
+        <Route  path="/friends">
+          <FriendsList />
+        </Route>
+        <Route  path="/friends/add">
+          <AddFriend />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
